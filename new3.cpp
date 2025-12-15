5d3cc633e953c13f6d67d19d8c4bedd033a78e55
shubham-vaishnav-13 <vaishnavshubham857@gmail.com>
2025-12-15 17:12:17 +0530

    added test 1

---
+++ new3.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    int tcase;
+    cin >> tcase;
+    while (tcase--)
+    {
+        
+    }
+    return 0;
+}
